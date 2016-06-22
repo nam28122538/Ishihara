c438f82198e170cbbb6f838c403bdcac6721dc3a
suttida <suttida5762110020@gmail.com>
2016-06-22 14:37:43 +0700

    ได้แล้วมีตัวเลขแล้ว

--- Ishihara/ViewController.swift
+++ Ishihara/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     
     
     
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +33,8 @@ class ViewController: UIViewController {
     
     
     @IBAction func answerButton(sender: AnyObject) {
+        
+        
         
         strAnswer = String(andexTextField.text)
         print("strAnswer ==> \(strAnswer)")
