4b67f1184dcfb5bfe55145bef5b3634bdc9a68cc
suttida <suttida5762110020@gmail.com>
2016-06-22 14:28:40 +0700

    answer button completed

--- Ishihara/ViewController.swift
+++ Ishihara/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ishiharalmagerView: UIImageView!
+    
+    @IBOutlet weak var andexTextField: UITextField!
+    //Explicit
+    var strAnswer:String = ""
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    }//Main Func
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func answerButton(sender: AnyObject) {
+        
+        strAnswer = String(andexTextField.text)
+        print("strAnswer ==> \(strAnswer)")
+        
+        
+        
+        
+    }//answerButton
+    
+    
+    
 
 
-}
+}//mainClass
 
